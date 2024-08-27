8c2bfa46291a0c868f5079ecca872c066aee80d6
Deepak Kumar Malik <deepakm661@gmail.com>
2024-08-27 08:35:57 +0530

    changed api route

--- internal/reverseimagegenerator/routes.go
+++ internal/reverseimagegenerator/routes.go
@@ -5,5 +5,5 @@ import "github.com/gin-gonic/gin"
 func (h *Handler) InitRoutes(router *gin.Engine) {
 	router.GET("/sanity", h.HellowWorldHandler)
 
-	router.POST("/reverse-image-generator", h.ReverseImageGenerator)
+	router.POST("/api/v1/reverse_image_generator", h.ReverseImageGenerator)
 }
